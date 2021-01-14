edf0ffa94d7da0871f69d390a8ba60bb40c693a8
Devjoyti <devjoytib@gmail.com>
2021-01-14 22:17:09 +0530

    Added userContext API

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Route, Switch } from "react-router-dom";
 import HomeScreen from "./components/homescreen/homescreen";
 import Products from "./components/homescreen/product/products";
@@ -8,17 +8,24 @@ import Pricing from "./components/homescreen/pricing/pricing";
 import Sidebar from "./components/homescreen/projectSidebar/Sidebar";
 import Header from "./components/homescreen/projectHeader/header";
 
+import { UserContext } from "./context/userContext/userContext";
+
 const App = () => {
+  const [user, setUser] = useContext(UserContext).user;
   return (
     <>
-      <Switch>
-        <Route exact path="/" component={HomeScreen} />
+      {user === null ? (
+        <Switch>
+          <Route exact path="/" component={HomeScreen} />
 
-        <Route path="/product" component={Products} />
-        <Route path="/resource" component={Resources} />
-        <Route path="/pricing" component={Pricing} />
-        <Route path="/signin" />
-      </Switch>
+          <Route path="/product" component={Products} />
+          <Route path="/resource" component={Resources} />
+          <Route path="/pricing" component={Pricing} />
+          <Route path="/signin" />
+        </Switch>
+      ) : (
+        <div>pokemon</div>
+      )}
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+
+import { UserContextProvider } from "./context/userContext/userContext";
 
 ReactDOM.render(
-    <Router>
-        <App/>
-    </Router>,
-    document.getElementById('root')
+  <Router>
+    <UserContextProvider>
+      <App />
+    </UserContextProvider>
+  </Router>,
+  document.getElementById("root")
 );
